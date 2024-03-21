21b46b89cc4d5fdd094e2202ae1a4d74985d9fb6
Covlea Pop <covleacc@gmail.com>
2024-03-21 15:26:43 +0200

    First

--- cypress/Integration/Examples/Test1.js
+++ cypress/Integration/Examples/Test1.js
@@ -6,7 +6,27 @@ describe('My First Test Suite', () => {
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
       cy.get('.search-keyword').type('ca')
       cy.wait(2000)
+      cy.get('.product').should('have.length',5)
       cy.get('.product:visible').should('have.length',4)
+      cy.get('.products').find('.product').should('have.length',4)
+      cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click()
+
+      cy.get('.products').find('.product').each(($el, index, $list) => {
+
+        const textVeg = $el.find('h4.product-name').text()
+        if(textVeg.includes('Cashews'))
+        {
+          cy.wrap($el).find('button').click()
+        }
+
+
+        })
+
+
+
+
+      })
+
 
 
 
@@ -15,4 +35,4 @@ describe('My First Test Suite', () => {
 
       
     })
-  })
+  
