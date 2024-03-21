86ae9038070dd1a8cc6f2ff88fac748f7cf8442b
Covlea Pop <covleacc@gmail.com>
2024-03-22 01:27:23 +0200

    First

--- cypress/Integration/Examples/Test1.js
+++ cypress/Integration/Examples/Test1.js
@@ -8,19 +8,30 @@ describe('My First Test Suite', () => {
       cy.wait(2000)
       cy.get('.product').should('have.length',5)
       cy.get('.product:visible').should('have.length',4)
-      cy.get('.products').find('.product').should('have.length',4)
-      cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click()
+      cy.get(".products").as("productlocator")
+      cy.get('@productlocator').find('.product').should('have.length',4)
+      cy.get('@productlocator').find('.product').eq(1).contains("ADD TO CART").click()
 
-      cy.get('.products').find('.product').each(($el, index, $list) => {
+      cy.get('@productlocator').find('.product').each(($el, index, $list) => {
 
         const textVeg = $el.find('h4.product-name').text()
         if(textVeg.includes('Cashews'))
         {
           cy.wrap($el).find('button').click()
         }
-        
+        })
+
+        // assert if logo text is correctly displayed
+          cy.get(".brand").should("have.text", "GREENKART")
+          
+        //this is to print to logs
+        cy.get(".brand").then(function(logoelement){
+
+          cy.log(logoelement.text())
+
 
         })
+        
 
       })
 
